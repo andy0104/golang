670494f468e4f93c5fffd90d9c911ae62b123722
Aninda Kar <aninda.kar@outlook.com>
2020-11-28 17:51:00 +0530

    cards project

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main()  {
-	
+import "fmt"
+
+func main() {
+	// Declaring a string variable
+	// var cards string = "Ace of Spades"
+
+	// This is similar to the above statement
+	// := should only be used during declaring a variable
+	cards := "A club of diamonds"
+
+	// Assigning a different value to cards var
+	cards = "A deck of cards"
+	fmt.Println(cards)
 }
